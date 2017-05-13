ae1c8366438e9ede5b02aed6d0dc9a5416f3d1b8
MadCoder <Huskygummyworms@users.noreply.github.com>
2017-05-12 21:31:41 -0400

    Update ThinClient.py

--- ThinClient.py
+++ ThinClient.py
@@ -3,11 +3,23 @@ import time
 import socket
 import subprocess
 
+sock = socket.socket()
+sock.bind(sock.gethostbyname(), 4545)
+
+
+
+
+
 def Tagging():
 	DIH = open("DeviceInfo.txt", "r+") #Device Info Handler
 	DI  = DIH.readlines() #Device Info
 	DeviceTag = DI[0]
 	EncryptionBool = DI[1]
 	MaxBytes = DI[2]
+	return DeviceTag, EncryptionBool, MaxBytes
+def Connect(sock):
+	sock.connect()# server ip adress
+	print("Connected to server, entering UI...")
+def UI():
 	
-Tagging()
+	
